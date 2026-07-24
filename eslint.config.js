8de3adac3946688622fa269ae11c09c8f4651530
Pethum Shyam <139078385+Pethumshyam6611@users.noreply.github.com>
2026-07-25 00:03:59 +0530

    chore: initialize React inventory project

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist', 'coverage'] },
+  { ...js.configs.recommended, files: ['**/*.{js,jsx}'] },
+  { ...reactHooks.configs.flat.recommended, files: ['**/*.{js,jsx}'] },
+  { ...reactRefresh.configs.vite, files: ['**/*.{js,jsx}'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+  },
+]
